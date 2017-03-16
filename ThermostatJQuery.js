f437a9be0d3e61abd2922ff1ed51575ebdbf49f2
Natalia Stafeeva <n.stafeeva@gmail.com>
2017-03-16 16:06:49 +0000

    Add energy usage indicator

---
+++ ThermostatJQuery.js
@@ -0,0 +1,71 @@
+    $( document ).ready(function() {
+      thermostat = new Thermostat;
+
+    $('.currentTemp').html(thermostat.currentTemp);
+
+    var usage = thermostat.energyUsage();
+
+    // function checkUsage() {
+    //   if (usage === "Low") {
+    //     $('.usage').css("background-color", "green");
+    //   }
+    //   else if (usage === "Medium") {
+    //     $('.usage').css("background-color", "blue");
+    //   }
+    //   else {
+    //     $('.usage').css("background-color", "red");
+    //   };
+    // };
+    // checkUsage();
+
+    $( 'button#up' ).click(function( event ) {
+      var usage = thermostat.energyUsage();
+      console.log(usage);
+      if (usage === "Low") {
+        $('.usage').css("background-color", "green");
+      }
+      else if (usage === "Medium") {
+        $('.usage').css("background-color", "blue");
+      }
+      else {
+        $('.usage').css("background-color", "red");
+      };
+      thermostat.increaseTemp();
+      $('.currentTemp').html(thermostat.currentTemp);
+      $('.thermometer:before').css("height", "60%");
+    });
+
+    $( 'button#down' ).click(function( event ) {
+      var usage = thermostat.energyUsage();
+      console.log(usage);
+      if (usage === "Low") {
+        $('.usage').css("background-color", "green");
+      }
+      else if (usage === "Medium") {
+        $('.usage').css("background-color", "blue");
+      }
+      else {
+        $('.usage').css("background-color", "red");
+      };
+      thermostat.decreaseTemp();
+      $('.currentTemp').html(thermostat.currentTemp);
+    });
+
+    $( 'button#reset' ).click(function( event ) {
+      thermostat.resetTemp();
+      $('.usage').css("background-color", "blue");
+      $('.currentTemp').html(thermostat.currentTemp);
+      thermostat.energySaving = true;
+      $(".psm").css("background-color", "green");
+    });
+
+    $( 'button#psm' ).click(function( event ) {
+      thermostat.toggleEnergySaving();
+      if (thermostat.energySaving === true) {
+        $("button#psm").css("background-color", "green");
+      }
+      else if (thermostat.energySaving === false) {
+        $("button#psm").css("background-color", "red");
+      };
+    });
+  });
